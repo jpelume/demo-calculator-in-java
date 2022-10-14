c3ec5be36a944d83d0e499b7832e37ce06a02845
jpelume <elumejohnpaulkemamukete@gmail.com>
2022-10-14 07:27:51 -0400

    first commit

--- CalDemo.java
+++ CalDemo.java
@@ -3,27 +3,71 @@ package Cal_demo;
 import java.util.Scanner; 
 
 public class CalDemo {
-
+	
 	public static void main(String[] args) {
-		int num1, num2 ;
-		//in oreder to take input from user we have to create scanner object
-		Scanner in = new Scanner(System.in);
-		System.out.println("Enter the first number: ");
+    Scanner in = new Scanner(System.in);
+    System.out.println("Enter the number(s) here: ");
+    int num = in.nextInt();
+    	
+    
+    System.out.println("Which operetion do you want to perform? (sum, sub, div, mul )");
+    
+    String operator = in.next();
+    
+    int result = 0;
+    int digit, sum = 0;
+    int mul = 1;
+    int sub = 0;
+    int div = 1;
+    int num1, num2;
+    
+   switch (operator) {
+   case "sum":
+	   do {
+		 digit = num % 10;
+		 sum = sum + digit;
+		 num = num / 10;
+	   } while (num > 0);
+	   result = sum;
+	   break;
+   case "sub":
+	   Scanner sc = new Scanner(System.in);
+	   System.out.println("Enter the first number here : ");
+	    num1 = sc.nextInt();
+	   System.out.println("Enter the second number here : ");
+	    num2 = sc.nextInt();
+	   
+		 sub = num1 - num2 ;
+			
+	   
+	   result = sub ;
+	   break;
+   case "div":
+	   Scanner sk = new Scanner(System.in);
+	   System.out.println("Enter the first number here : ");
+	    num1 = sk.nextInt();
+	   System.out.println("Enter the second number here : ");
+	    num2 = sk.nextInt();
+	    
+	    div = num1 / num2 ;
+	    
+	    result = div ;
+	   break;
+   case "mul":
+	   do {
+		   digit = num % 10;
+			 mul = mul * digit;
+			 num = num / 10;
+	   }while (num > 0);
+	   result = mul ;
+	   break;
+	   
+	default :
+		System.out.println("sorry! operator is incorrect");
 		
-		num1 = in.nextInt();
-		
-		System.out.println("Enter the second number: ");
-		
-		num2 = in.nextInt();
-		
-		System.out.println("Sum = "+(num1+num2));
-		System.out.println("Subtraction = "+(num1-num2));
-		System.out.println("Multiplication = "+(num1*num2));
-        System.out.println("Division = "+(num1/num2));
-
-		
-		
-
+   }
+    
+   System.out.println( "the answer is" + " " + result ); 
+    
 	}
-
 }
